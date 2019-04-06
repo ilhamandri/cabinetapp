aefed48de5942a6ce7172504539e28307e480643
ilhamandri <ilhamandriandri@gmail.com>
2019-04-06 18:26:40 +0700

    add documents controler method

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,32 @@
+class DocumentsController < ApplicationController
+
+	def index
+
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def create
+	end
+
+	def destroy
+	end
+
+	private
+
+		def find_doc
+		end
+
+		def doc_params
+		end
+end
