0e55c9cf6fda35aa5c7a4d34fab864cda5955afd
ilhamandri <ilhamandriandri@gmail.com>
2019-04-07 19:15:26 +0700

    change root of application for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
-
   resources :documents
+  authenticated :user do
+  	root "documents#index", as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 end
