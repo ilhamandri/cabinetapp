acd92e275594579f738d4de6b74b2e322ff7c7c1
ilhamandri <ilhamandriandri@gmail.com>
2019-04-07 18:44:44 +0700

    install devise

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,32 +1,50 @@
 class DocumentsController < ApplicationController
+	before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
 	def index
-
+		@docs = Document.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@doc = Document.new
 	end
 
 	def edit
 	end
 
 	def update
+		if @doc.update(doc_params)
+			redirect_to @doc
+		else
+			render 'edit'
+		end
 	end
 
 	def create
+		@doc = Document.new(doc_params)
+
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 	end
 
 	def destroy
+		@doc.destroy
+		redirect_to documents_path
 	end
 
 	private
 
 		def find_doc
+			@doc = Document.find(params[:id])
 		end
 
 		def doc_params
+			params.require(:document).permit(:title,:content)
 		end
 end
